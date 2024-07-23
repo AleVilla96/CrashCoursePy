7485310168c4067447d614ee2b4845d152edb0eb
Ale <ale.villa.0296@gmail.com>
2024-07-22 23:17:16 -0300

    Primer commit

--- restaurant.py
+++ restaurant.py
@@ -1,18 +1,31 @@
 class Restaurant:
+    '''
+    Initialization method
+    Name, Cuisine, Status, Clients served
+    '''
+
     def __init__(self, res_name, res_cuisine):
         self.res_name = res_name
         self.res_cuisine = res_cuisine
         self.isOpen = False
+        self.number_served = 0
 
     '''Using this method we invert the state of the restaurant using the not operator (review this operator)'''
     def openOrClose(self):
         self.isOpen = not self.isOpen
 
-    '''Here we use the ternary operator to print the actual state of the restaurant'''
+    def set_number_served(self, num):
+        self.number_served = num
+
+    def increment_number_served(self, num):
+        self.number_served += num
+
+    '''Here we use the ternary operator to print the actual state of the restaurant (open or closed)'''
     def describeRes(self):
         print(f"Restaurant Name: {self.res_name}")
         print(f"Cuisine: {self.res_cuisine}")
         print("Open\n" if self.isOpen else "Closed\n")
+
 
 
 res1 = Restaurant("Havana", "Chinese")
@@ -24,3 +37,9 @@ res2.describeRes()
 
 res3 = Restaurant("Patel", "Hindi")
 res3.describeRes()
+
+print(f"Served: {res1.number_served}")
+res1.set_number_served(44)
+res1.increment_number_served(2)
+print(f"Served: {res1.number_served}")
+
---
+++ users.py
@@ -0,0 +1,35 @@
+class User:
+    def __init__(self, first_name, last_name, age, doc_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.doc_number = doc_number
+        self.login_attempts = 0
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_ogin_attempts(self):
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print(f"First Name: {self.first_name}")
+        print(f"Last Name: {self.last_name}")
+        print(f"Age: {self.age}")
+        print(f"Doc Number: {self.doc_number}")
+
+    def greet_user(self):
+        print(f"Greetings {self.first_name} {self.last_name}!!")
+
+usr1 = User("Ale", "Villa", 28, 39796715)
+usr1.greet_user()
+usr1.describe_user()
+
+usr1.increment_login_attempts()
+usr1.increment_login_attempts()
+usr1.increment_login_attempts()
+usr1.increment_login_attempts()
+
+print(f"Login Attempts: {usr1.login_attempts}")
+usr1.reset_ogin_attempts()
+print(f"Login Attempts: {usr1.login_attempts}")
